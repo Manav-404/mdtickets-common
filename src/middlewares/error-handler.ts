3231e67ffdb934dce8de96e36a5f7ed7b3d68e27
Manav-404 <manavdesai30399@gmail.com>
2023-05-20 20:37:00 +0530

    fix: added a default error code and message if not customerror instance

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -6,4 +6,6 @@ export const errorHandler =
     if(err instanceof CustomError){
         return res.status(err.statusCode).send({errors: err.serializeErrors()})
     }
+
+    return res.status(400).send({errors: [{message: 'Something went wrong'}]})
 }
